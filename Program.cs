1dc9d66f9c668c210d3bbf31dd9f3f640b71b762
DiogoMeida <diogo.meida@gmail.com>
2023-01-23 15:44:02 -0300

    cryptography using C#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+
+namespace testeCriptoConsole
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            
+            string wordToBeCrypted = "teste";
+            
+            //encrypting
+            var byteArray = Encoding.ASCII.GetBytes(wordToBeCrypted);
+            var wordCrypted = Convert.ToBase64String(byteArray);
+            Console.WriteLine(wordCrypted);
+
+            //decrypting
+            var wordToBeDecrypted = "dGVzdGU=";
+            var dadosAsBytes = System.Convert.FromBase64String(wordToBeDecrypted);
+            var wordDecrypted = System.Text.ASCIIEncoding.ASCII.GetString(dadosAsBytes);
+            Console.WriteLine(wordDecrypted);
+        }
+    }
+}
